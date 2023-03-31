c52abdd9ef7158e244566988642de7417fa1a43d
Naranbat <naranbat@jnaya.com>
2023-03-31 18:18:58 +0800

    deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv';
+dotenv.config()
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -17,19 +19,16 @@ const config: HardhatUserConfig = {
     // hardhat: {
     //   chainId: 31337,
     // },
-    localhost: {
-      chainId: 1337,
-    },
     bsctestnet: {
       chainId: 97,
       url: process.env.BSC_TESTNET_RPC_URL,
-      accounts: [process.env.BSC_TESTNET_PRIVATE_KEY!],
+      accounts: [process.env.BSC_TESTNET_DEPLOYER_KEY!],
     },
-    // bscmainnet: {
-    //   chainId: 56,
-    //   url: process.env.BSC_MAINNET_RPC_URL,
-    //   accounts: [process.env.BSC_MAINNET_PRIVATE_KEY!],
-    // },
+    bscmainnet: {
+      chainId: 56,
+      url: process.env.BSC_MAINNET_RPC_URL,
+      accounts: [process.env.BSC_MAINNET_DEPLOYER_KEY!],
+    },
   },
 };
 
--- scripts/dlt30-deploy.ts
+++ scripts/dlt30-deploy.ts
@@ -6,9 +6,6 @@ async function main() {
     const _contract = await Contract.deploy();
     await _contract.deployed();
     console.log(`${contractName} - contract deployed to ${_contract.address}`);
-
-    // process.env.BSC_TESTNET_RPC_URL,
-    console.log(``);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
